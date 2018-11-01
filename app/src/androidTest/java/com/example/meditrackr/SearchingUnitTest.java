296d3cc689b09ad32b52c60c839d7e999f28e0c9
gfroilan <froilan@ualberta.ca>
2018-11-01 14:26:27 -0600

    SearchingUnitTest commit(2/3)-assertions probably fixed

--- app/src/androidTest/java/com/example/meditrackr/SearchingUnitTest.java
+++ app/src/androidTest/java/com/example/meditrackr/SearchingUnitTest.java
@@ -11,48 +11,69 @@ import static org.junit.Assert.*;
 public class SearchingUnitTest {
     /**
      * Unit test for search functionality
+     * Structure based off JUnit Test Lab Slides and lonelyTwitter
      */
 
-    private final String[] exampleKeywords = {"Weird bruise", "Orange finger",
+    private final String[] exampleGeneralKeywords = {"Weird bruise", "Orange finger",
             "Blue Rash", "Itchy Ear"};
+    private final Strig[] exampleLocationKeywords = {Canada, US, Singapore,
+            Japan, Brazil, South Africa};
+    private Keyword keyword = new Keyword();
 
+    // Constructor method for SearchingUnitTest
+    public SearchingUnitTest() {
+        super(com.example.meditrackr.Searching.class);
+    }
+
+    // First 3 methods are for general keyword search
     // Checks whether a keyword was entered for a search
     // Search input line should not be left blank
-    public void blankSearchTest() {
-        final String generalKeyword = "";
-        if (generalKeyword.length() == 0) {
-            assertNull("Search input line cannot be left blank", generalKeyword);
-        }
+    public void blankGeneralSearchTest() {
+        Keyword generalKeyword = new GeneralKeyword("");
+        keyword.add(generalKeyword);
+        assertTrue(keyword.length() == 0);
     }
 
     // Checks whether the keyword search is able to match up to another string
-    // If keyword doesn't exists in array, search should not be able to match
-    public void checkKeywordTest() {
-        final String generalKeyword = "Black tongue";
-        for(generalKeyword : exampleKeywords){
-            if(exampleKeywords.contains(generalKeyword)){
-                assertArrayEquals("Black tongue", "Black tongue");
-            }else{
-                assertNotEquals("There are no existing records/problems that" +
-                        " match with this keyword", generalKeyword);
-            }
-        }
+    // If general keyword doesn't exists in array, search should not be able to match
+    public void dontMatchKeywordTest() {
+        Keyword generalKeyword = new GeneralKeyword("Black tongue");
+        keyword.add(generalKeyword);
+        assertFalse(exampleGeneralKeywords.hasKeyword(keyword));
     }
 
     // Checks whether the keyword search is able to match up to another string
-    // If keyword exists in array, search should be able to match
-    public void checkKeywordTest() {
-        final String generalKeyword = "Itchy Ear";
-        for(generalKeyword : exampleKeywords){
-            if(exampleKeywords.contains(generalKeyword)){
-                assertArrayEquals("Itchy Ear", "Itchy Ear");
-            }else{
-                assertNotEquals("There are no existing records/problems that" +
-                        " match with this keyword", generalKeyword);
-            }
-        }
+    // If general keyword exists in array, search should be able to match
+    public void matchKeywordTest() {
+        Keyword generalKeyword = new GeneralKeyword("Itchy Ear");
+        keyword.add(generalKeyword);
+        assertTrue(exampleGeneralKeywords.hasKeyword(keyword));
     }
 
-    //up to here is part 1/3 of unit test code
-    //still have yet to figure out how to actually use assertions in testing
-}
+   // Next 3 methods are for Geo-location keywords
+   // Search input line for Geo-location shoul not be left blank
+   public void blankGeoLocationSearchTest() {
+        Keyword locationKeyword = new LocationKeyword("");
+        keyword.add(locationKeyword);
+        assertTrue(keyword.length() == 0);
+   }
+
+    // Checks whether the keyword search is able to match up to another string
+    // If location keyword doesn't exists in array, search should not be able to match
+    public void dontMatchKeywordTest() {
+        Keyword locationKeyword = new LocationKeyword("Sweden");
+        keyword.add(locationKeyword);
+        assertFalse(exampleLocationKeywords.hasKeyword(keyword));
+    }
+
+    // Checks whether the keyword search is able to match up to another string
+    // If location keyword exists in array, search should be able to match
+    public void matchKeywordTest() {
+        Keyword locationKeyword = new LocationKeyword("South Africa");
+        keyword.add(locationKeyword);
+        assertTrue(exampleLocationKeywords.hasKeyword(keyword));
+
+    // 2/3rds of searching unit test
+
+
+    }

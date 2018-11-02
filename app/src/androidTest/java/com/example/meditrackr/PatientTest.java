bd64e1bf124ce8155e160b01a55c41a5c464a250
chatterl <35278416+chatterl@users.noreply.github.com>
2018-11-02 12:25:59 -0600

    Update PatientTest.java

--- app/src/androidTest/java/com/example/meditrackr/PatientTest.java
+++ app/src/androidTest/java/com/example/meditrackr/PatientTest.java
@@ -1,1 +1,46 @@
+/*
+*********************
+READ ME:
+Right now we have no concrete testes for this class that are neither redundant nor trivial.
+therefore we will write some test cases for the future that we think we may need
+*********************
 
+package com.example.meditrackr;
+
+import com.example.meditrackr.models.CareProvider;
+import com.example.meditrackr.models.Patient;
+import com.example.meditrackr.models.Problem;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class PatientTest {
+    /**
+     * Unit Test for patient
+     */
+
+    // Check if the patient changes the doctor and it can recognize that
+    // patient will get no feed back if they have no doctor
+    @Test
+    public void checkNewDoc() {
+        final Patient tempPatient =
+        final  ArrayList<Doctor> docList = new ArrayList<>();
+        final Patient tempPatient = new Patient
+                ("", "", "", "", docList);
+      // replace a doctor
+      // check to see if that old doctor is in the array (it should int be)
+      //chack to see if the new doc is there (they should be)
+
+    }
+
+
+
+
+    }
+}
+
+
+*/

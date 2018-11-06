c54d8eeee13a349761765a30687eef0861dd9d76
skryt <cokan@ualberta.ca>
2018-11-05 19:08:02 -0700

    update

--- app/src/main/java/com/example/meditrackr/ui/MainActivity.java
+++ app/src/main/java/com/example/meditrackr/ui/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.meditrackr.ui;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -9,18 +11,22 @@ import com.example.meditrackr.models.CareProvider;
 import com.example.meditrackr.models.Patient;
 import com.example.meditrackr.R;
 
-public class MainActivity extends Activity {
+import io.searchbox.core.SearchResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    final ImageView problems = (ImageView) findViewById(R.id.problems);
+    final ImageView map = (ImageView) findViewById(R.id.map);
+    final ImageView camera = (ImageView) findViewById(R.id.camera);
+    final ImageView search = (ImageView) findViewById(R.id.search);
+    final ImageView profile = (ImageView) findViewById(R.id.profile);
+    final Bundle bundle = getIntent().getExtras();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ImageView problems = (ImageView) findViewById(R.id.problems);
-        final ImageView map = (ImageView) findViewById(R.id.map);
-        final ImageView camera = (ImageView) findViewById(R.id.camera);
-        final ImageView search = (ImageView) findViewById(R.id.search);
-        final ImageView profile = (ImageView) findViewById(R.id.profile);
 
         problems.setImageDrawable(getResources().getDrawable(R.drawable.cross_full));
 
@@ -47,6 +53,7 @@ public class MainActivity extends Activity {
                 }
                 else{
                     image.setImageDrawable(getResources().getDrawable(R.drawable.person_full));
+                    FragmentTransition(v);
                 }
             }
         };
@@ -56,15 +63,43 @@ public class MainActivity extends Activity {
         search.setOnClickListener(listener);
         profile.setOnClickListener(listener);
 
-
-        Bundle bundle = getIntent().getExtras();
-
         if(bundle.get("doctor") != null ){
             CareProvider careProvider = (CareProvider) bundle.getSerializable("careProvider");
 
         }
         else{
             Patient patient = (Patient) bundle.getSerializable("patient");
+        }
+    }
+
+    public void FragmentTransition(View v){
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.addToBackStack(null);
+        if(v == problems){
+            ProblemsFragment fragment = ProblemsFragment.newInstance();
+            transaction.replace(R.id.content, fragment);
+            transaction.commit();
+        }
+        else if (v == map){
+            MapFragment fragment = MapFragment.newInstance();
+            transaction.replace(R.id.content, fragment);
+            transaction.commit();
+        }
+        else if (v == camera) {
+            CameraFragment fragment = CameraFragment.newInstance();
+            transaction.replace(R.id.content, fragment);
+            transaction.commit();
+        }
+        else if (v == search){
+            CameraFragment fragment = CameraFragment.newInstance();
+            transaction.replace(R.id.content, fragment);
+            transaction.commit();
+        }
+        else {
+            UserFragment fragment = UserFragment.newInstance();
+            transaction.replace(R.id.content, fragment);
+            transaction.commit();
         }
     }
 

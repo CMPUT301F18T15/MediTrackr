90b6503a9689fa38ee83ac5e215a63ffeb94d151
chatterl <chatterl@ualberta.ca>
2018-11-14 15:47:41 -0700

    javadocs camerafragment

--- app/src/main/java/com/example/meditrackr/ui/patient/CameraFragment.java
+++ app/src/main/java/com/example/meditrackr/ui/patient/CameraFragment.java
@@ -8,6 +8,12 @@ import android.view.ViewGroup;
 
 import com.example.meditrackr.R;
 
+/**
+ * @return  fragment
+ * @author  Orest Cokan
+ * @version 1.0 Nov 12, 2018.
+ */
+
 public class CameraFragment extends Fragment {
 
     public static CameraFragment newInstance(){

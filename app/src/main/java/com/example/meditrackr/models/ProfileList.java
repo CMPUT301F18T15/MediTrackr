7227512256cf301e98674720908a5d44115aa5bc
chatterl <chatterl@ualberta.ca>
2018-11-14 11:29:33 -0700

    javadoc profilelist

--- app/src/main/java/com/example/meditrackr/models/ProfileList.java
+++ app/src/main/java/com/example/meditrackr/models/ProfileList.java
@@ -3,9 +3,14 @@ package com.example.meditrackr.models;
 import java.util.ArrayList;
 
 /**
- * Created by Skryt on Nov 7, 2018
+ * this class creates a ProfileList which stores all profiles in one place for the database to use.
+ * this class can add a profile, remove a profile, check to see if a profile exists.
+ * find out the number of profiles in the list.
+ *
+ * @parama ArrayList<profile>      creates a new empty list to put profiles in
+ * @author  Orest Cokan
+ * @version 1.0 Nov 7, 2018.
  */
-
 public class ProfileList {
     private ArrayList<Profile> profiles = new ArrayList<>();
 

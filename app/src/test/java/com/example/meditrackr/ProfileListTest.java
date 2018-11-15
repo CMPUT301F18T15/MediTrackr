0c6fdea27753bc5e8b0e35c74d7fdd5fb95b9c87
m-usman11 <usman1@ualberta.ca>
2018-11-15 11:45:37 -0700

    ProfileList tests

---
+++ app/src/test/java/com/example/meditrackr/ProfileListTest.java
@@ -0,0 +1,55 @@
+package com.example.meditrackr;
+
+import com.example.meditrackr.models.Profile;
+import com.example.meditrackr.models.ProfileList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit tests for ProfileList
+ */
+
+public class ProfileListTest {
+
+    private ProfileList profileList;
+
+    @Before
+    public void newProfileList() {
+        profileList = new ProfileList();
+    }
+
+    @Test
+    public void addProfileTest() {
+        final Profile tempProfile = new Profile
+                ("", "", "", "", false);
+        profileList.addProfile(tempProfile);
+        assertTrue("Profile not added to ProfileList",
+                profileList.size() != 0);
+    }
+
+    @Test
+    public void removeProfileTest() {
+        final Profile tempProfile = new Profile
+                ("", "", "", "", false);
+        profileList.addProfile(tempProfile);
+        profileList.removeProfile(tempProfile);
+        assertTrue("Profile not removed from ProfileList",
+                profileList.size() == 0);
+    }
+
+    @Test
+    public void hasProfileTest() {
+        final Profile tempProfile = new Profile
+                ("", "", "", "", false);
+        profileList.addProfile(tempProfile);
+        assertTrue("Profile not added to ProfileList",
+                profileList.containsProfile(tempProfile));
+
+        profileList.removeProfile(tempProfile);
+        assertFalse("Profile not removed from ProfileList",
+                profileList.containsProfile(tempProfile));
+    }
+}

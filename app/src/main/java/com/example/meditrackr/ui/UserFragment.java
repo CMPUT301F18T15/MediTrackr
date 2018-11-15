4786b3a473706a3c7e448aa1f1ef9e898269cef2
gfroilan <froilan@ualberta.ca>
2018-11-15 16:12:40 -0700

    some ui comments-commit

--- app/src/main/java/com/example/meditrackr/ui/UserFragment.java
+++ app/src/main/java/com/example/meditrackr/ui/UserFragment.java
@@ -18,6 +18,7 @@
  */
 package com.example.meditrackr.ui;
 
+//imports
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -34,59 +35,57 @@ import com.example.meditrackr.controllers.ProfileManager;
 import com.example.meditrackr.models.Profile;
 import com.example.meditrackr.ui.patient.AddRecordFragment;
 
-
 /**
  * this fragment shows the users username, phone number, email and an image of the patient if they added one
- * there is also a butto that will take the user to an edit profile fragment (UserEditFragment)
+ * there is also a button that will take the user to an edit profile fragment (UserEditFragment)
  * @author  Orest Cokan
  * @version 2.0 Nov 4, 2018.
  * @see UserEditFragment
  */
 
+// Class displays user profile
 public class UserFragment extends Fragment {
+    // Set variables
     Profile profile = ProfileManager.getProfile();
     public static UserFragment newInstance(){
         UserFragment fragment = new UserFragment();
         return fragment;
     }
 
+    // Creates view for fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_user, container, false);
 
+        // Get fragment bundle
         final Bundle bundle = getArguments();
 
-        // set ui definitions
+        // Set ui definitions
         ImageView user_image = rootView.findViewById(R.id.patient_image);
         TextView username = rootView.findViewById(R.id.patient_username);
         TextView email = rootView.findViewById(R.id.patient_email);
         TextView phone = rootView.findViewById(R.id.patient_phone);
         Button editButton = rootView.findViewById(R.id.edit_button);
 
+        // Display username, email, and phone number
         username.setText(profile.getUsername());
         email.setText(profile.getEmail());
         phone.setText(profile.getPhone());
 
+        // Clicking on button allows user to switch to edit profile fragment
         editButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FragmentManager manager = getFragmentManager();
-                FragmentTransaction transaction = manager.beginTransaction();
-                transaction.addToBackStack(null);
-                UserEditFragment fragment = UserEditFragment.newInstance().newInstance();
-                transaction.replace(R.id.content, fragment);
-                transaction.commit();
+                FragmentTransaction transaction = manager.beginTransaction(); // Allow editing on fragment
+                transaction.addToBackStack(null); // Allows user to bring back previous fragment when back button is pressed
+                UserEditFragment fragment = UserEditFragment.newInstance().newInstance(); // Creates new instance of user edit fragment
+                transaction.replace(R.id.content, fragment); // Replace current fragment with new info
+                transaction.commit(); // Make permanent all changes made in the transaction
             }
         });
-
-
-
-
-
-
-
 
         return rootView;
     }

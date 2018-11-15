8c6974fb2d8da3d5ec4abe344250fed9f1f3c73a
gfroilan <froilan@ualberta.ca>
2018-11-15 04:52:03 -0700

    hahahahaha ElasticSearchController documentation and license fix-commit ahaha

--- app/src/main/java/com/example/meditrackr/adapters/careprovider/ProblemAdapter.java
+++ app/src/main/java/com/example/meditrackr/adapters/careprovider/ProblemAdapter.java
@@ -1,22 +1,19 @@
 /*
- *           Apache 2.0 License Notice
+ *Apache 2.0 License Notice
  *
- *Licensed to the Apache Software Foundation (ASF) under one
- *or more contributor license agreements.  See the NOTICE file
- *distributed with this work for additional information
- *regarding copyright ownership.  The ASF licenses this file
- *to you under the Apache License, Version 2.0 (the
- *"License"); you may not use this file except in compliance
- *with the License.  You may obtain a copy of the License at
-
- *  http://www.apache.org/licenses/LICENSE-2.0
-
- *Unless required by applicable law or agreed to in writing,
- *software distributed under the License is distributed on an
- *"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *KIND, either express or implied.  See the License for the
- *specific language governing permissions and limitations
- *under the License.
+ *Copyright 2018 CMPUT301F18T15
+ *
+ *Licensed under the Apache License, Version 2.0 (the "License");
+ *you may not use this file except in compliance with the License.
+ *You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *Unless required by applicable law or agreed to in writing, software
+ *distributed under the License is distributed on an "AS IS" BASIS,
+ *WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *See the License for the specific language governing permissions and
+ *limitations under the License.
  *
  */
 package com.example.meditrackr.adapters.careprovider;
@@ -43,6 +40,7 @@ import com.example.meditrackr.ui.careprovider.ProblemsFragment;
  * Version 1.0
  * careprovider\ProblemAdapter.java creates the RecyclerView Adapter for the Problem list
  * RecyclerView for careprovider accounts
+ *
  */
 
 public class ProblemAdapter extends RecyclerView.Adapter<ProblemAdapter.ViewHolder> {

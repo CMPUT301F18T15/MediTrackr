e94e0879e6121bd2fdf40c969dbb3d04c2d7bfef
m-usman11 <usman1@ualberta.ca>
2018-11-16 03:56:04 -0700

    Add GeoLocationTest file

---
+++ app/src/test/java/com/example/meditrackr/GeoLocationTest.java
@@ -0,0 +1,14 @@
+package com.example.meditrackr;
+
+import com.example.meditrackr.models.record.Geolocation;
+
+import static org.junit.Assert.*;
+
+/**
+ * GeoLocation Unit Tests
+ */
+
+public class GeoLocationTest {
+    private Geolocation geolocation;
+}
+

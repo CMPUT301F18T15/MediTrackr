3d32085f4e0fc6e2a223195ab01ea35725f4dc39
m-usman11 <usman1@ualberta.ca>
2018-11-16 12:35:24 -0700

    GeoLocation tests

--- app/src/test/java/com/example/meditrackr/GeoLocationTest.java
+++ app/src/test/java/com/example/meditrackr/GeoLocationTest.java
@@ -2,7 +2,10 @@ package com.example.meditrackr;
 
 import com.example.meditrackr.models.record.Geolocation;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 
 /**
  * GeoLocation Unit Tests
@@ -10,5 +13,30 @@ import static org.junit.Assert.*;
 
 public class GeoLocationTest {
     private Geolocation geolocation;
+
+    @Test
+    public void setGeoLocationTest() {
+        final double initLatitude = 20;
+        final double initLongitude = 10;
+        final String initAddr = "A";
+        geolocation = new Geolocation(initLatitude, initLongitude, initAddr);
+
+        assertEquals(initLatitude, geolocation.getLatitude(), 0.0);
+        assertEquals(initLongitude, geolocation.getLongitude(), 0.0);
+        assertEquals(initAddr, geolocation.getAddress());
+
+        final double newLatitude = 40;
+        final double newLongitude = 30;
+        final String newAddr = "B";
+
+        geolocation.setLatitude(newLatitude);
+        geolocation.setLongitude(newLongitude);
+        geolocation.setAddress(newAddr);
+
+        assertEquals(newLatitude, geolocation.getLongitude(), 0.0);
+        assertEquals(newLongitude, geolocation.getLongitude(), 0.0);
+        assertEquals(newAddr, geolocation.getAddress());
+    }
+
 }
 

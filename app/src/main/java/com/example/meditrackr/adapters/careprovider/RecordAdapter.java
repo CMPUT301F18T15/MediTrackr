d1f831258dbe125e6018a9aa8df7a4c38facea3d
gfroilan <froilan@ualberta.ca>
2018-11-16 20:38:56 -0700

    licenses and clean up doc-commit

--- app/src/main/java/com/example/meditrackr/adapters/careprovider/RecordAdapter.java
+++ app/src/main/java/com/example/meditrackr/adapters/careprovider/RecordAdapter.java
@@ -15,6 +15,8 @@
  *See the License for the specific language governing permissions and
  *limitations under the License.
  *
+<<<<<<< HEAD
+=======
  */
 package com.example.meditrackr.adapters.careprovider;
 

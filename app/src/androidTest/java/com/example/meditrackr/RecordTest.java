8aa3bf8990120ed4f3cb78fcc59b341a71e10614
m-usman11 <usman1@ualberta.ca>
2018-11-19 15:23:19 -0700

    Adding two records test

--- app/src/androidTest/java/com/example/meditrackr/RecordTest.java
+++ app/src/androidTest/java/com/example/meditrackr/RecordTest.java
@@ -1,17 +1,24 @@
 package com.example.meditrackr;
 
+import android.app.Activity;
 import android.app.Instrumentation;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.NoMatchingViewException;
+import android.support.test.espresso.intent.Intents;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.rule.ActivityTestRule;
+import android.support.v4.widget.TextViewCompat;
 import android.util.Log;
 
 import com.example.meditrackr.controllers.ElasticSearchController;
 import com.example.meditrackr.ui.LoginActivity;
 import com.example.meditrackr.ui.MainActivity;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Rule;
@@ -27,6 +34,9 @@ import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -66,6 +76,7 @@ public class RecordTest extends ActivityTestRule<MainActivity> {
         }
     }
 
+
     @Test
     public void testANewProblem() {
         onView(withId(R.id.add_problem_floating)).perform(click());
@@ -98,6 +109,47 @@ public class RecordTest extends ActivityTestRule<MainActivity> {
             fail("Record was added without a name");
         }
 
+        Espresso.pressBack();
+        Espresso.pressBack();
+        onView(withId(R.id.problem_delete_button)).perform(click());
+        onView(withText("YES")).perform(click());
+    }
+
+
+    @Test
+    public void testBTwoRecords() {
+        onView(withId(R.id.add_problem_floating)).perform(click());
+        onView(withId(R.id.problem_title_field)).perform(click(), typeText(problemName), pressBack());
+        onView(withId(R.id.problem_description_field)).perform
+                (click(), closeSoftKeyboard(), typeText(problemDesc), pressBack());
+        onView(withId(R.id.problem_add_button)).perform(click());
+        onView(withId(R.id.problem_description)).perform(click());
+        onView(withId(R.id.add_record_floating)).perform(click());
+        try { // Allow accessing location
+            onView(withText("ALLOW")).perform(click());
+        } catch (NoMatchingViewException e) {}
+
+        // First Record
+        onView(withId(R.id.record_title_field)).perform(click(), typeText(recordName), pressBack());
+        onView(withId(R.id.record_description_field)).perform(scrollTo(), click(), typeText(recordDesc), pressBack());
+        onView(withId(R.id.add_record_button)).perform(scrollTo(), click());
+        try {
+            onView(withId(R.id.add_record_floating)).check(matches(isDisplayed()));
+        } catch (NoMatchingViewException e) {
+            fail("Record was added without a name");
+        }
+
+        // Second Record
+        onView(withId(R.id.add_record_floating)).perform(click());
+        try { // Allow accessing location
+            onView(withText("ALLOW")).perform(click());
+        } catch (NoMatchingViewException e) {}
+        onView(withId(R.id.record_title_field)).perform(click(), typeText("WATERRr"), pressBack());
+        onView(withId(R.id.record_description_field))
+                .perform(scrollTo(), click(), typeText("Press F to pay respects"), pressBack());
+        onView(withId(R.id.add_record_button)).perform(scrollTo(), click());
+        
+        Espresso.pressBack();
         Espresso.pressBack();
         Espresso.pressBack();
         onView(withId(R.id.problem_delete_button)).perform(click());

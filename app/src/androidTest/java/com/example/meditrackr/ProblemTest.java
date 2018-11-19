06ba9145beefe980059af8462ad123cbdf35a10d
m-usman11 <usman1@ualberta.ca>
2018-11-19 04:12:55 -0700

    All Problem tests pass

--- app/src/androidTest/java/com/example/meditrackr/ProblemTest.java
+++ app/src/androidTest/java/com/example/meditrackr/ProblemTest.java
@@ -3,19 +3,12 @@ package com.example.meditrackr;
 import android.app.Instrumentation;
 import android.content.Context;
 import android.content.Intent;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.AmbiguousViewMatcherException;
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.NoMatchingViewException;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.rule.ActivityTestRule;
 
-import com.example.meditrackr.adapters.patient.ProblemAdapter;
 import com.example.meditrackr.controllers.ElasticSearchController;
-import com.example.meditrackr.controllers.SaveLoadController;
-import com.example.meditrackr.models.Patient;
-import com.example.meditrackr.models.Problem;
-import com.example.meditrackr.models.ProblemList;
 import com.example.meditrackr.ui.LoginActivity;
 import com.example.meditrackr.ui.MainActivity;
 
@@ -24,18 +17,19 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
-import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.*;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
+
 
 public class ProblemTest extends ActivityTestRule<MainActivity> {
 
     final String problemName = "Hydrophobia";
-    final String problemDesc = "I am terrified of water!!";
+    final String problemDesc = "Water D:";
     final String testPatientName = "InstrumentationTestPatient";
     Instrumentation.ActivityMonitor activityMonitor;
 
@@ -66,18 +60,6 @@ public class ProblemTest extends ActivityTestRule<MainActivity> {
                     (click(), typeText("InstrumentationTestPatient"), pressBack());
             onView(withId(R.id.login_button)).perform(click());
 
-            // Delete all problems
-            boolean end = false;
-            while (!end) {
-                try {
-                    onView(withId(R.id.problem_delete_button)).check(matches(isDisplayed()));
-                } catch (AmbiguousViewMatcherException | NoMatchingViewException e) {
-                    if (e instanceof AmbiguousViewMatcherException)
-                        onData(withId(R.id.problem_delete_button)).atPosition(0).perform(click());
-                    else if (e instanceof  NoMatchingViewException)
-                        end = true;
-                }
-            }
         }
     }
 
@@ -117,16 +99,65 @@ public class ProblemTest extends ActivityTestRule<MainActivity> {
 
 
     @Test
-    public void editProblemTest() {
+    public void deleteProblemTest() {
+        // Create problem
         onView(withId(R.id.add_problem_floating)).perform(click());
         onView(withId(R.id.problem_title_field)).perform(click(), typeText(problemName), pressBack());
         onView(withId(R.id.problem_description_field)).perform
                 (click(), closeSoftKeyboard(), typeText(problemDesc), pressBack());
         onView(withId(R.id.problem_add_button)).perform(click());
 
+        try {
+            onView(withId(R.id.add_problem_floating)).check(matches(isDisplayed()));
+        } catch (NoMatchingViewException e) {
+            fail("Problem was not added");
+        }
 
+        onView(withId(R.id.problem_delete_button)).perform(click());
+        onView(withText("YES")).perform(click());
+
+        try {
+            onView(withId(R.id.add_problem_floating)).check(matches(isDisplayed()));
+        } catch (NoMatchingViewException e) {
+            fail("Problem was not deleted");
+        }
 
     }
+
+
+    @Test
+    public void editProblemTest() {
+        // Create problem
+        onView(withId(R.id.add_problem_floating)).perform(click());
+        onView(withId(R.id.problem_title_field)).perform(click(), typeText(problemName), pressBack());
+        onView(withId(R.id.problem_description_field)).perform
+                (click(), closeSoftKeyboard(), typeText(problemDesc), pressBack());
+        onView(withId(R.id.problem_add_button)).perform(click());
+
+        try {
+            onView(withId(R.id.add_problem_floating)).check(matches(isDisplayed()));
+        } catch (NoMatchingViewException e) {
+            fail("Problem was not added");
+        }
+
+        final String appendTtile = " CRITICAL";
+        final String appendDesc = " Very very terrified!!!!";
+        onView(withId(R.id.problem_edit_button)).perform(click());
+        onView(withId(R.id.edit_problem_title_field))
+                .perform(click(), typeText(appendTtile), pressBack());
+        onView(withId(R.id.edit_problem_description_field))
+                .perform(click(), closeSoftKeyboard(), typeText(appendDesc), pressBack());
+        onView(withId(R.id.edit_problem_save_button)).perform(click());
+
+        try {
+            onView(withText(problemName + appendTtile)).check(matches(isDisplayed()));
+            onView(withText(problemDesc + appendDesc)).check(matches(isDisplayed()));
+        } catch (NoMatchingViewException e) {
+            fail("Problem was not edited");
+        }
+
+    }
+
 
     // Creates the test patient account in case it wasn't created
     private void createTestPatient() {
@@ -139,4 +170,5 @@ public class ProblemTest extends ActivityTestRule<MainActivity> {
         onView(withId(R.id.Patient)).perform(click());
         onView(withId(R.id.signup_button)).perform(click());
     }
+    
 }

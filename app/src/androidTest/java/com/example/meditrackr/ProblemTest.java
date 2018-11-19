f9da3e6b640c5e176676646ea1208aa3eae9a4bb
m-usman11 <usman1@ualberta.ca>
2018-11-19 03:48:22 -0700

    Partial edit test

--- app/src/androidTest/java/com/example/meditrackr/ProblemTest.java
+++ app/src/androidTest/java/com/example/meditrackr/ProblemTest.java
@@ -1,13 +1,21 @@
 package com.example.meditrackr;
 
 import android.app.Instrumentation;
+import android.content.Context;
 import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.AmbiguousViewMatcherException;
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.NoMatchingViewException;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.rule.ActivityTestRule;
 
+import com.example.meditrackr.adapters.patient.ProblemAdapter;
 import com.example.meditrackr.controllers.ElasticSearchController;
+import com.example.meditrackr.controllers.SaveLoadController;
+import com.example.meditrackr.models.Patient;
+import com.example.meditrackr.models.Problem;
+import com.example.meditrackr.models.ProblemList;
 import com.example.meditrackr.ui.LoginActivity;
 import com.example.meditrackr.ui.MainActivity;
 
@@ -16,11 +24,9 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.pressBack;
-import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.action.ViewActions.*;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -44,13 +50,14 @@ public class ProblemTest extends ActivityTestRule<MainActivity> {
     @Before
     public void reset() {
 
+        Context instrumentationCtx;
+
         if (ElasticSearchController.searchProfile(testPatientName) == null) {
             createTestPatient();
 
         } else {
 
             // Login to testPatient
-
             activityMonitor = getInstrumentation().addMonitor
                     (MainActivity.class.getName(), null, false);
             Intent start = new Intent();
@@ -58,6 +65,19 @@ public class ProblemTest extends ActivityTestRule<MainActivity> {
             onView(withId(R.id.patient_username)).perform
                     (click(), typeText("InstrumentationTestPatient"), pressBack());
             onView(withId(R.id.login_button)).perform(click());
+
+            // Delete all problems
+            boolean end = false;
+            while (!end) {
+                try {
+                    onView(withId(R.id.problem_delete_button)).check(matches(isDisplayed()));
+                } catch (AmbiguousViewMatcherException | NoMatchingViewException e) {
+                    if (e instanceof AmbiguousViewMatcherException)
+                        onData(withId(R.id.problem_delete_button)).atPosition(0).perform(click());
+                    else if (e instanceof  NoMatchingViewException)
+                        end = true;
+                }
+            }
         }
     }
 
@@ -93,6 +113,19 @@ public class ProblemTest extends ActivityTestRule<MainActivity> {
 
         // Return to Main
         Espresso.pressBack();
+    }
+
+
+    @Test
+    public void editProblemTest() {
+        onView(withId(R.id.add_problem_floating)).perform(click());
+        onView(withId(R.id.problem_title_field)).perform(click(), typeText(problemName), pressBack());
+        onView(withId(R.id.problem_description_field)).perform
+                (click(), closeSoftKeyboard(), typeText(problemDesc), pressBack());
+        onView(withId(R.id.problem_add_button)).perform(click());
+
+
+
     }
 
     // Creates the test patient account in case it wasn't created

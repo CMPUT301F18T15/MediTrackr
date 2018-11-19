2facc08d06a1a7fce11e6cb8b80267d432dd0fc7
m-usman11 <usman1@ualberta.ca>
2018-11-19 01:26:58 -0700

    All register tests pass

--- app/src/androidTest/java/com/example/meditrackr/RegisterTest.java
+++ app/src/androidTest/java/com/example/meditrackr/RegisterTest.java
@@ -4,36 +4,22 @@ import android.app.Activity;
 import android.app.Instrumentation;
 import android.content.Context;
 import android.content.Intent;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.Espresso;
-import android.support.test.espresso.intent.*;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.rule.ActivityTestRule;
-import android.util.Log;
-import android.widget.EditText;
 
-import com.example.meditrackr.controllers.ElasticSearchController;
 import com.example.meditrackr.models.Patient;
 import com.example.meditrackr.ui.LoginActivity;
 import com.example.meditrackr.ui.MainActivity;
-import com.example.meditrackr.ui.RegisterFragment;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-
-import java.io.File;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.pressBack;
-import static android.support.test.espresso.action.ViewActions.swipeUp;
 import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.action.ViewActions.typeTextIntoFocusedView;
-import static android.support.test.espresso.intent.Intents.intended;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 import static org.junit.Assert.*;
@@ -93,7 +79,14 @@ public class RegisterTest extends ActivityTestRule<LoginActivity> {
 
     @Test
     public void emailEmptyTest() {
+        final String emptyEmail = "";
+        onView(withId(R.id.not_member)).perform(click());
+        onView(withId(R.id.patient_username)).perform(click(), typeText("Long enough name"), pressBack());
+        onView(withId(R.id.phone_number)).perform(click(), typeText(patientPhone), pressBack());
+        onView(withId(R.id.signup_button)).perform(click());
 
+        assertNull("Passed with empty phone",
+                getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 5));
     }
 
 }

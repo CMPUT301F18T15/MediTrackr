073c5b955af99fada30063d48d84be748c192328
gfroilan <froilan@ualberta.ca>
2018-11-27 10:39:48 -0700

    documentation

--- app/src/main/java/com/example/meditrackr/ui/patient/MapActivity.java
+++ app/src/main/java/com/example/meditrackr/ui/patient/MapActivity.java
@@ -404,11 +404,8 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         }
     };
 
-<<<<<<< HEAD
-    // Gets location info from location found
-=======
 
->>>>>>> master
+    // Gets location info from location found
     private ResultCallback<PlaceBuffer> mUpdatePlaceDetailsCallback = new ResultCallback<PlaceBuffer>() {
         @Override
         public void onResult(@NonNull PlaceBuffer places) {
@@ -486,13 +483,10 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                     hideSoftKeyboard();
                 }
             }
-<<<<<<< HEAD
         }else { // Else if there are no marker do not add markers
             Log.d("Adding no materkers", "adding no markers");
-=======
         }else {
             Log.d("Adding no markers", "adding no markers");
->>>>>>> master
         }
 
     }

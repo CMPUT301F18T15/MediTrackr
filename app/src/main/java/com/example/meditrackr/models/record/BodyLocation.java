9804f829c37ce6cebcc85b3e408e3ed8e8898a63
Veronica <vsalm@ualberta.ca>
2018-12-03 14:19:37 -0700

    Added basic headers

--- app/src/main/java/com/example/meditrackr/models/record/BodyLocation.java
+++ app/src/main/java/com/example/meditrackr/models/record/BodyLocation.java
@@ -1,10 +1,11 @@
-/*
- * BodyLocation
+/*--------------------------------------------------------------------------
+ * FILE: BodyLocation.java
  *
- * Version 1.0
- * Oct 24, 2018.
+ * PURPOSE: Holds all body location information stored about a record,
+ *          including the photo name, photoID, and position on the
+ *          photo.
  *
- * Apache 2.0 License Notice
+ *     Apache 2.0 License Notice
  *
  * Copyright 2018 CMPUT301F18T15
  *
@@ -12,7 +13,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,7 +21,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ --------------------------------------------------------------------------*/
 
 package com.example.meditrackr.models.record;
 
